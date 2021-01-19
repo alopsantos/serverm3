d63be249e068e12d07b10871315885f7bb883bf1
alopsantos <anderson_lops@hotmail.com>
2021-01-19 20:48:10 -0300

    type orm

---
+++ src/database/migrations/1611100030942-create_imoveil.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createImoveil1611100030942 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
+
+app.use(express.json());
+
+/* app.get('/users', () => {
+    console.log('teste');
+}) */
 
 app.listen(5555);
