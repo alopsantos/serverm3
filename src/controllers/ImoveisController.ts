284d93e753931fdc7d941a89e5caa99b2306fe21
alopsantos <anderson_lops@hotmail.com>
2021-01-21 13:05:58 -0300

    :sparkles: cors

--- src/controllers/ImoveisController.ts
+++ src/controllers/ImoveisController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
-import * as Yup from 'Yup';
+import * as Yup from 'yup';
 
 import imovelView from "../views/imoveis_views";
 import Imovel from "../models/Imovel";
--- src/errors/handler.ts
+++ src/errors/handler.ts
@@ -9,14 +9,15 @@ const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
   if (error instanceof ValidationError) {
     let errors: IValidationErrors = {};
 
-    error.inner.forEach(err => {
-      errors[err.path] = err.errors;
+    error.inner.forEach((err) => {
+      //errors[err.path] = err.errors;
     });
 
-    return response.status(400).json({ message: "Validation fails ", errors });
+    return response.status(400).json({ message: "Validation fails.", errors });
   }
+  console.error(error);
 
-  return response.status(500).json({ message: "Internal server error" });
+  return response.status(500).json({ message: "Internal server error." });
 };
 
 export default errorHandler;
